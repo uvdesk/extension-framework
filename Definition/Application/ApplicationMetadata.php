aab4222e2c73ae66a85ed72e88c4bba3efda3251
akshay kumar <akshay.kumar758@webkul.com>
2019-08-14 18:48:35 +0530

    Update application metadata definition

--- Definition/Application/ApplicationMetadata.php
+++ Definition/Application/ApplicationMetadata.php
@@ -4,16 +4,12 @@ namespace Webkul\UVDesk\ExtensionFrameworkBundle\Definition\Application;
 
 abstract class ApplicationMetadata
 {
+    public abstract function getName() : string;
+
+    public abstract function getQualifiedName() : string;
+
     public function getIconPath() : string
     {
         return '';
     }
-
-    public abstract function getName() : string;
-
-    public abstract function getSummary() : string;
-
-    public abstract function getDescription() : string;
-
-    public abstract function getQualifiedName() : string;
 }
