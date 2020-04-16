01a54445a956789fdf3012371b198e3a6fa44e66
sanjeev <sanjeev.papnoi829@webkul.com>
2020-04-16 14:30:06 +0530

    changed procees of translation to speedup the process

--- UIComponents/Dashboard/Homepage/Sections/Apps.php
+++ UIComponents/Dashboard/Homepage/Sections/Apps.php
@@ -8,12 +8,12 @@ class Apps extends HomepageSection
 {
     public static function getTitle() : string
     {
-        return self::dynamicTranslation("Apps");
+        return "Apps";
     }
 
     public static function getDescription() : string
     {
-        return self::dynamicTranslation("Integrate apps as per your needs to get things done faster than ever");
+        return "Integrate apps as per your needs to get things done faster than ever";
     }
 
     public static function getRoles() : array
